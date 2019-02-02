a2b2e0aae8320cc29eb1a2999de6d2c6713e6882
user <novak.andriy92@gmail.com>
2019-02-02 14:03:30 +0200

    Create CredentialsRepo interface.

---
+++ app/src/main/java/com/anovak92/passwordholder/model/CredentialsRepo.java
@@ -0,0 +1,12 @@
+package com.anovak92.passwordholder.model;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface CredentialsRepo {
+
+    Map<Integer, Credentials> loadCredentials() throws IOException;
+
+    void saveCredentials() throws IOException;
+
+}
