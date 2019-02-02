63b5c120fd26f7ffdc4dc515dd05b1ace670a468
user <novak.andriy92@gmail.com>
2019-02-02 14:01:35 +0200

    Create CredentialsView interface.

--- app/src/main/java/com/anovak92/passwordholder/CredentialsView.java
+++ app/src/main/java/com/anovak92/passwordholder/CredentialsView.java
@@ -3,6 +3,16 @@ package com.anovak92.passwordholder;
 
 public interface CredentialsView {
 
+    enum Mode {
+        CREATE,
+        VIEW,
+        EDIT
+    }
 
+    void setMode(Mode mode);
+
+    Mode getCurrentMode();
+
+    void save();
 
 }
